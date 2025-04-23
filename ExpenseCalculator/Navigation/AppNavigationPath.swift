8fd98be21de1b6e164b9c1e074857dc31abed564
Gaurang Lathiya <gaurang.lathiya8@gmail.com>
2025-04-23 23:37:57 +0530

    Remove extra code

--- ExpenseCalculator/Navigation/AppNavigationPath.swift
+++ ExpenseCalculator/Navigation/AppNavigationPath.swift
@@ -18,38 +18,4 @@ enum AppMainNavigationPath {
     case addExpense
 }
 
-extension AppMainNavigationPath {
-    
-    var name: String {
-        switch self {
-        case .verifyOtp(_, _):
-            return "verifyOtp"
-        default:
-            return String(describing: self)
-        }
-    }
-    
-//    // This will be dynamic based on different flow - and calculate based on requirement. - only useful for popToScreen
-//    var index: Int {
-//        switch self {
-//        case .login:
-//            return 1
-//        case .verifyOtp(_ , _):
-//            return 2
-//        case .home:
-//            return 1
-//        case .setting:
-//            return 2
-//        case .accountSetting:
-//            return 3
-//        case .expenseList:
-//            return 2
-//        case .expenseDetail:
-//            return 3
-//        case .addExpense:
-//            return 2
-//        }
-//    }
-}
-
 extension AppMainNavigationPath: Hashable { }
